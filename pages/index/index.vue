a7612bd3b5b1163c51ac0207484839387b063bbb
yuanyxh <15766118362@139.com>
2023-11-18 11:55:37 +0800

    feat: 跳转代码结构

--- pages/index/index.vue
+++ pages/index/index.vue
@@ -1,16 +1,8 @@
 <template>
-  <view class="index">
-    <scroll-view-bounce class="bounce" :scroll-y="true">
-      <view class="card" v-for="item in 25">
-        {{ item }}
-      </view>
-    </scroll-view-bounce>
-  </view>
+  <view class="index"> </view>
 </template>
 
 <script>
-import ScrollViewBounce from "/components/scroll-view-bounce/scroll-view-bounce.vue";
-
 export default {
   data() {
     return {};
@@ -23,21 +15,4 @@ export default {
 };
 </script>
 
-<style scoped>
-* {
-  margin: 0;
-  padding: 0;
-}
-
-.bounce {
-  height: calc(100vh - 44px);
-}
-
-.card {
-  margin-top: 10px;
-  background-color: yellowgreen;
-  height: 80px;
-  border-top: 1px solid #ddd;
-  border-bottom: 1px solid #ddd;
-}
-</style>
+<style></style>
